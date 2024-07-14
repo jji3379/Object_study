8a176facf5d65dde06cf5822c60c958d0fa59061
jji3379 <jji3379@naver.com>
2024-07-15 01:54:25 +0900

    refactor: 캡슐화를 적용한 코드(데이터를 처리하는 데 필요한 메서드를 데이터를 가지고 있는 객체 스스로 구현하고 있다. => 따라서 이 객체들은 스스로를 책임진다고 말할 수 있다.)

--- src/main/java/com/example/object_study/chapter04/Screening.java
+++ src/main/java/com/example/object_study/chapter04/Screening.java
@@ -1,5 +1,6 @@
 package com.example.object_study.chapter04;
 
+import com.example.object_study.chapter02.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,4 +12,22 @@ public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateNoneDiscountedFee().times(audienceCount);
+                }
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }
